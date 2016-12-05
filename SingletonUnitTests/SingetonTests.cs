eb83d2480ed9359b3b19a2fd9437e62a3d7aae74
Shubhra Mittal <shub_goel@hotmail.com>
2016-12-05 09:37:36 -0800

    Added SingeltonUnitTests

---
+++ SingletonUnitTests/SingetonTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Singleton;
+using System.Threading;
+
+namespace SingletonUnitTests
+{
+    [TestClass]
+    public class SingetonTests
+    {
+
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string s1;
+            string s2;
+            testContextInstance.WriteLine("Hello");
+
+            new Thread(() =>
+
+            {
+                MyfabClass myfabc;
+
+                myfabc = MyfabClass.GiveMeInstance();
+                s1 = myfabc.identifier;
+                testContextInstance.WriteLine($"Came to this:{s1}");
+                Console.WriteLine(myfabc.identifier);
+            }
+            );
+
+            new Thread(() =>
+
+            {
+                MyfabClass myfabc;
+
+                myfabc = MyfabClass.GiveMeInstance();
+                s2 = myfabc.identifier;
+                testContextInstance.WriteLine(s2);
+                Console.WriteLine(myfabc.identifier);
+            }
+            );
+
+            //Assert.AreNotEqual(s1, s2);
+
+
+        }
+    }
+}
